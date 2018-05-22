391b3cd6b2655b4e2ca9abbd2e5dff9fd394eb53
Jack (Booua) Paciorek <jack.paciorek@gmail.com>
2018-05-22 15:19:28 +0200

    add function for adding more obstacles to the frame

--- main.js
+++ main.js
@@ -10,10 +10,19 @@ function draw() {
   background(0);
   dino.renderDino();
   dino.updateDinoPosition();
+
   for (obstacle of obstaclesArray) {
     console.log(obstacle);
     obstacle.renderObstacle();
     obstacle.updateObstacle();
+  }
+  
+  this.addObstacleToFrame();
+}
+
+function addObstacleToFrame(){
+  if(frameCount % 100 == 0){
+    obstaclesArray.push(new CactusObstacle());
   }
 }
 
